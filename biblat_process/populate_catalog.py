328a5761fa40f8afd4cfe4909fd5a03f8f2c286c
Arturo Rendón Cruz <arendon@dgb.unam.mx>
2019-02-25 15:56:02 -0600

    Proceso para guardar de datos fuente para Idioma en BD

--- biblat_process/populate_catalog.py
+++ biblat_process/populate_catalog.py
@@ -5,7 +5,7 @@ import os
 import json
 import logging
 
-from biblat_schema.catalogs import Pais
+from biblat_schema.catalogs import Pais, Idioma
 from mongoengine import connect
 
 from biblat_process.settings import config
@@ -17,12 +17,13 @@ LOGGER_FMT = '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
 
 connect(db=config.MONGODB_NAME, host=config.MONGODB_HOST)
 
+
 class PopulateCatalog:
     def __init__(self):
         self.data_dir = os.path.join(SCRIPT_PATH, '../datos')
         self.files = {
             'Pais': 'Pais.json',
-            'Idioma': 'Idioma.tsv',
+            'Idioma': 'Idioma.json',
             'TipoDocumento': 'TipoDocumento.tsv',
             'EnfoqueDocumento': 'EnfoqueDocumento.tsv',
             'Disciplina': 'Disciplina.json',
@@ -33,11 +34,28 @@ class PopulateCatalog:
         }
 
     def pais(self):
-        with open(os.path.join(self.data_dir, self.files['Pais']), encoding="utf-8") as jsonf:
+        with open(os.path.join(self.data_dir, self.files['Pais']),
+                  encoding="utf-8") as jsonf:
             paises = json.load(jsonf)
             for pais_data in paises:
-                pais = Pais(**pais_data)
-                pais.save()
+                try:
+                    pais = Pais(**pais_data)
+                    pais.save()
+                except Exception as e:
+                    logging.error('Error al procesar %s' % str(pais_data))
+                    logging.error('%s' % str(e))
+
+    def idioma(self):
+        with open(os.path.join(self.data_dir, self.files['Idioma']),
+                  encoding="utf-8") as jsonf:
+            idiomas = json.load(jsonf)
+            for idioma_data in idiomas:
+                try:
+                    idioma = Idioma(**idioma_data)
+                    idioma.save()
+                except Exception as e:
+                    logging.error('Error al procesar %s' % str(idioma_data))
+                    logging.error('%s' % str(e))
 
 
 def main():
@@ -57,7 +75,7 @@ def main():
         '--catalog',
         '-a',
         default='all',
-        choices=['pais', 'all'],
+        choices=['pais', 'idioma', 'all'],
         help='Seleccione proceso a ejecutar'
     )
 
@@ -71,6 +89,9 @@ def main():
 
     if args.catalog in ('pais', 'all'):
             populate_catalog.pais()
+
+    if args.catalog in ('idioma', 'all'):
+            populate_catalog.idioma()
 
 
 if __name__ == "__main__":
